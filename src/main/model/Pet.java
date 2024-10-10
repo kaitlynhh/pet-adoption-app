e15b35658053339137c688fe550536ba9d9a598f
dhao21 <dhao21@student.ubc.ca>
2024-10-10 15:18:56 -0700

    add pet class w/ stub and pet tests

---
+++ src/main/model/Pet.java
@@ -0,0 +1,85 @@
+package model;
+
+// Creates a pet and it's available for adoption.
+public class Pet {
+    private int id;
+    private String name;
+    private int age;
+    private String species;
+    private String breed;
+    private String adoptionStatus;
+
+    // Constructor
+    public Pet(int id, String name, int age, String species, String breed) {
+        // TODO: contructor stub
+    }
+
+    //getters
+    public int getId() {
+        //stub
+        return 0;
+    }
+    
+    public String getPetName() {
+        // stub
+        return "";
+    }
+
+    public int getPetAge() {
+        //stub
+        return 0;
+    }
+
+    public String getSpecies() {
+        //stub
+        return "";
+    }
+
+    public String getBreed() {
+        //stub
+        return "";
+    }
+
+    // EFFECTS: return "Available" or "Adopted"
+    public String getAdoptionStatus() {
+        //stub
+        return "";
+    }
+
+    // REQUIRES: pet must be available.
+    // MODIFIES: this
+    // EFFECTS: change the adoption status of the pet.
+    public void markAsAdopted() {
+        // stub
+    }
+
+    // EFFECTS: return true if the pet is available for adoption.
+    public boolean isAvailable() {
+        //stub
+        return true;
+    }
+
+    //setters
+    public void setName(String name) {
+        this.name = name; 
+    }
+
+    public void setAge(int age) { 
+        this.age = age; 
+    }
+
+    public void setSpecies(String species) {
+        this.species = species; 
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed; 
+    }
+
+    public void setStatus(String status) {
+        this.adoptionStatus = status;
+    }
+}
+
+
+
---
+++ src/test/model/PetTest.java
@@ -0,0 +1,46 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import model.Pet;
+
+
+public class PetTest {
+
+    private Pet testPet;
+
+    @BeforeEach
+    void runBefore() {
+        testPet = new Pet(1, "Otto",2, "Dog", "Golden");
+
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals(1, testPet.getId());
+        assertEquals("Otto", testPet.getPetName());
+        assertEquals(2, testPet.getPetAge());
+        assertEquals("Dog", testPet.getSpecies());
+        assertEquals("Golden", testPet.getBreed());
+        assertEquals("available", testPet.getAdoptionStatus());
+    }
+
+    @Test
+    void testMarkAsAdopted() {
+        testPet.markAsAdopted();
+        assertEquals("adopted", testPet.getAdoptionStatus());
+    }
+
+    @Test
+    void testIsAvailable() {
+        assertTrue(testPet.isAvailable());
+        testPet.markAsAdopted();
+        assertFalse(testPet.isAvailable());
+    }
+
+
+}
+
