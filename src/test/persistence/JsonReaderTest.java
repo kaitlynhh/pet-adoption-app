78484c2649d8dc578ae6dc4db15c7ef64f4662e1
dhao21 <dhao21@student.ubc.ca>
2024-10-21 16:30:30 -0700

    reader test added

---
+++ src/test/persistence/JsonReaderTest.java
@@ -0,0 +1,54 @@
+package persistence;
+
+import model.AdoptApplication;
+import model.User;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.experimental.categories.Category;
+
+public class JsonReaderTest extends JsonTest {
+
+    @Test
+    void testReaderNonExistentFile() {
+        JsonReader reader = new JsonReader("./data/noSuchFile.json");
+        try {
+            User user = reader.read();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void testReaderNoApplication() {
+        JsonReader reader = new JsonReader("./data/testReaderNoApplication.json");
+        try {
+            User user = reader.read();
+            assertEquals("Lars", user.getName());
+            assertEquals(0, user.getApplications().size());
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    void testReaderGeneralUser() {
+        JsonReader reader = new JsonReader("./data/testReaderGeneralUser.json");
+        try {
+            User user = reader.read();
+            assertEquals("Adopter", user.getName());
+            List<AdoptApplication> applications = user.getApplications();
+            assertEquals(2, applications.size());
+            checkApplication("adopter", "Otto", "submitted", applications.get(0));
+            checkApplication("Adopter", "Bob", "submitted", applications.get(1));
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+}
