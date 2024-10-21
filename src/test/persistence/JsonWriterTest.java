ac868903b19a018d50827b84ede9c20c352d4481
dhao21 <dhao21@student.ubc.ca>
2024-10-21 16:30:20 -0700

    writer test added

---
+++ src/test/persistence/JsonWriterTest.java
@@ -0,0 +1,70 @@
+package persistence;
+
+import model.User;
+import model.AdoptApplication;
+
+import org.junit.experimental.categories.Category;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JsonWriterTest extends JsonTest {
+    @Test
+    void testWriterInvalidFile() {
+        try {
+            User user = new User("Lars","Adopter");
+            JsonWriter writer = new JsonWriter("./data/my\0illegal:fileName.json");
+            writer.open();
+            fail("IOException was expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void testWriterNoApplication() {
+        try {
+            User user = new User("Lars", "Adopter");
+            JsonWriter writer = new JsonWriter("./data/testWriterNoApplication.json");
+            writer.open();
+            writer.write(user);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterNoApplication.json");
+            user = reader.read();
+            assertEquals("Lars", user.getName());
+            assertEquals("Adopter", user.getRole());
+            assertEquals(0, user.getApplications().size());
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+
+    @Test
+    void testWriterGeneralUser() {
+        try {
+            User user = new User("Lars", "Adopter");
+            user.submitApplication(new AdoptApplication("Lars", "Otto"));
+            user.submitApplication(new AdoptApplication("Lars", "Bob"));
+            JsonWriter writer = new JsonWriter("./data/testWriterGeneralUser.json");
+            writer.open();
+            writer.write(user);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterGeneralUser.json");
+            user = reader.read();
+            assertEquals("Lars", user.getName());
+            List<AdoptApplication> applications = user.getApplications();
+            assertEquals(2, applications.size());
+            checkApplication("Lars", "Otto", "submitted", applications.get(0));
+            checkApplication("needle", "Bob", "submitted", applications.get(1));
+
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+
+}
